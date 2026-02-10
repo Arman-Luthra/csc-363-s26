9600ae3e2fc71a9c061334b375797a52dde0a4ed
arman-luthra <aluthra26@wabash.edu>
2026-02-09 19:12:15 -0500

    PA2 snapshot

--- pa3/generate-tests.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for acfile in tests/*.ac; do
-    base="${acfile%.ac}"
-    dcfile="${base}.dc"
-
-    echo "Running: acdc.py $acfile $dcfile"
-    python3 acdc.py "$acfile" "$dcfile"
-done
